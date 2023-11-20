d23ae0e7a07a11c1b2e3585bd1950c50d0f1c600
Rubens César <38004191+rubenscesar@users.noreply.github.com>
2023-11-20 08:18:52 -0300

    Update Backup dos Bancos do P.bat

--- 11 - MANUAIS, LEIAUTES, RECURSOS/Backup dos Bancos do P.bat
+++ 11 - MANUAIS, LEIAUTES, RECURSOS/Backup dos Bancos do P.bat
@@ -17,24 +17,30 @@ set dia=%data:~0,2%
 set hora=%time%
 set hh=%hora:~0,2%
 set mm=%hora:~3,2%
-echo Guardando backup dos bancos do %P_Pronto%.
+echo Guardando backup dos bancos do %P_Pronto% em %PathDestino%.
 
 set file1=%P_Pronto%\AC.bak
 set file2=%PathDestino%\CopiaRealizadaEm_%dia%%mes%%ano%_As_%hh%h%mm%m__AC.bak
 copy %file1% %file2% /Y
+echo Arquivo copiado: %file2%
+set file3="%P_Pronto%\AC.fbk"
+set file4="%PathDestino%\CopiaRealizadaEm_%dia%%mes%%ano%_As_%hh%h%mm%m__AC.fbk"
+copy %file3% %file4% /Y
+echo Arquivo copiado: %file4%
+set file5="%P_Pronto%\AC_bak.fbk"
+set file6="%PathDestino%\CopiaRealizadaEm_%dia%%mes%%ano%_As_%hh%h%mm%m__AC_bak.fbk"
+copy %file5% %file6% /Y
+echo Arquivo copiado: %file6%
+set file7="%P_Pronto%\cs9.cds"
+set file8="%PathDestino%\CopiaRealizadaEm_%dia%%mes%%ano%_As_%hh%h%mm%m__cs9.cds"
+copy %file7% %file8% /Y
+echo Arquivo copiado: %file8%
+echo.
+echo.
 call CompareArqHashes.bat %file1% %file2%
-set file1="%P_Pronto%\AC.fbk"
-set file2="%PathDestino%\CopiaRealizadaEm_%dia%%mes%%ano%_As_%hh%h%mm%m__AC.fbk"
-copy %file1% %file2% /Y
-call CompareArqHashes.bat %file1% %file2%
-set file1="%P_Pronto%\AC_bak.fbk"
-set file2="%PathDestino%\CopiaRealizadaEm_%dia%%mes%%ano%_As_%hh%h%mm%m__AC_bak.fbk"
-copy %file1% %file2% /Y
-call CompareArqHashes.bat %file1% %file2%
-set file1="%P_Pronto%\cs9.cds"
-set file2="%PathDestino%\CopiaRealizadaEm_%dia%%mes%%ano%_As_%hh%h%mm%m__cs9.cds"
-copy %file1% %file2% /Y
-call CompareArqHashes.bat %file1% %file2%
+call CompareArqHashes.bat %file3% %file4%
+call CompareArqHashes.bat %file5% %file6%
+call CompareArqHashes.bat %file7% %file8%
 CLS
 echo.
 echo Copia realizada com sucesso!
